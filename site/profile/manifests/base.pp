3d578d38927464fbcb1e6442a17d224ccf38632a
root <root@criticalhojo1c.mylabserver.com>
2019-01-16 18:43:12 +0000

    added the correct profiles to site dir

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,17 @@
-class profile::base {
+class profile::base (
+  $ntp_servers = [
+  '0.ubuntu.pool.ntp.org',
+  '1.ubuntu.pool.ntp.org',
+  '2.ubuntu.pool.ntp.org',
+  ],
+){
+  
+  include ::ssh
+  class  { '::ntp':  
+    servers => $ntp_servers,
+  }
 
-  #the base profile should include component modules that will be on all nodes
-
+  if $facts['os']['family'] == 'RedHat' {
+    include ::profile::selinux
+  }
 }
